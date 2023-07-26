d670b068951783c3ae48e07e2b8061f5ba68b5f4
MateusFoppa <mateusfoppa@outlook.com>
2023-07-25 22:52:04 -0300

    :recycle: refactor server

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,9 @@ import app from './app';
 import connectDB from './db/connect';
 
 const port = process.env.PORT || 5000;
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    console.log(`Banco de dados conectado: ${process.env.DB_NAME}`);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
 
-start();
+app.listen(port, async () => {
+  await connectDB(process.env.MONGO_URI);
+  console.log(`Banco de dados conectado: ${process.env.DB_NAME}`);
+  console.log(`Server is listening on port ${port}...`)
+})
